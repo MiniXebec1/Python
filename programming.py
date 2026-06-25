53fc6211b8ac4c36d0a7f059057cc70e93be8a06
MiniXebec1 <magizhanskrp@gmail.com>
2026-06-25 20:04:49 +0800

    Add files via upload

---
+++ programming.py
@@ -0,0 +1,11 @@
+print('hello', end='\nhii')
+print('hello'+'how are you?')
+a3=10
+print(a3)
+HI4HI=43
+print(HI4HI)
+#COMMENT it is taking error because special characters are not allowed in variable names and spaces only underscore is allowed in variable names
+#@=10
+#print(@)
+#ACTIVITY 3- KEY WORDS: use only for python/cennot be used for user purpose
+help('keywords')
