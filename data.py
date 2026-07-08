1411337ed86268a17b7605e507641fe177f9f743
MiniXebec1 <magizhanskrp@gmail.com>
2026-07-08 20:13:00 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,40 @@
+# data:information/values which are stored
+import string
+
+
+a=34 
+b='magizhan'
+print(type(a))
+# integer:34,45,
+# string:hello,hi
+#float:2.3,10.4
+#boolean:True,False
+
+c= 3.5
+print(type(c))
+a=35
+
+a1=float(a)
+print(a1)
+print(type(a1))
+
+z=8
+z1=int(z)
+print(z1)
+print(type(z1))
+
+
+f=23.4
+print(type(f))
+f1=int(f)
+print(f1)
+print(type(f1))
+
+text1=str(input('ben'))
+revText=text1[::-1]
+text1=revText
+print('text1')
+
+
+
+
